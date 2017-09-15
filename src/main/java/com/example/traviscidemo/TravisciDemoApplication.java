f25311a040f98b701a2824d6c3e36fe883034c3b
pvtyuan <pvtyuan@foxmail.com>
2017-09-15 14:28:43 +0800

    Initial commit

---
+++ src/main/java/com/example/traviscidemo/TravisciDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.traviscidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisciDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravisciDemoApplication.class, args);
+	}
+}
